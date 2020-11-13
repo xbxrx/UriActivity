8a4907a80840e7ede5ae42cc0265accaf2cdf049
xbxrx <1152091362@qq.com>
2020-11-13 21:26:08 +0800

    第二次更新 旋转Button

--- app/src/main/java/com/example/uriactivity/SecondActivity.java
+++ app/src/main/java/com/example/uriactivity/SecondActivity.java
@@ -1,7 +1,11 @@
 package com.example.uriactivity;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,10 +21,35 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
         button=findViewById(R.id.button);
         tx=findViewById(R.id.hobby);
+        final ValueAnimator animator = ValueAnimator.ofFloat(0,360);
+        animator.setDuration(500);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(final ValueAnimator animation) {
+
+                        button.setRotation((float)animation.getAnimatedValue());
+            }
+        });
+        animator.setRepeatCount(10);
+        animator.setRepeatMode(ValueAnimator.REVERSE);
+
+        animator.start();
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+//                button.animate().rotation(360).setDuration(500).start();
+                if (animator.isRunning())
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                        animator.pause();
+                    }
+                else {
+                    if (animator.isStarted())
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                            animator.resume();
+                        }
+                    else
+                        animator.start();
+                }
             }
         });
 
